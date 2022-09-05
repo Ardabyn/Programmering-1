9cbc35f9b119e9d70d78fc2201f95bd532c68f9d
Arda Kop <arda.kop@elev.ga.ntig.se>
2022-09-05 13:07:41 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# Variabler
+# Sparar texten hampuse1 i variabeln username
+username = Arda
+
+
+# Sparar integern 28 i variabeln age
+age = 17
+
+# Ökar värdet på variabeln age med 1
+age += 1
+
+# print
+print(username)
+print(age)
+
+# input - sparar som str
+name = input("Hej, vad heter du? ")
+
+print("Välkommen", name, "till våran skola. Du är", age, "gammal.")
+
+newage = int(input("Hur gammal är du?"))
+print("Om 5 år är du", newage+5, "år gammal.")
+
+# Datatyper
+# str - string - text
+"Kossa"
+
+# int - integer - heltal
+leet = 1337
+
+# float - decimaltal
+leetfloat = 13.37
+
+print(leet+leetfloat)
