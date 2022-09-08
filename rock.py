813a513726ddea0effd0f0337810ee825b8916a0
Arda Kop <arda.kop@elev.ga.ntig.se>
2022-09-08 12:30:39 +0200

    fadfhah

---
+++ rock.py
@@ -0,0 +1,17 @@
+import random
+
+computer = random.choice(["rock", "paper", "scissors"]).capitalize()
+
+user = input("rock, paper or scissors? ").capitalize()
+
+print ("The computer chose", computer,"and the user chose", user +".")
+
+if computer == ("Scissors") and user == "Paper":
+    print ("The computer wins")
+
+elif computer == ("Rock") and user == "Rock":
+    print ("You have tied")
+
+
+
+# TODO - Implement the if statements that prints who wins
